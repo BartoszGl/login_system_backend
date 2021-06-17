190920802bfef1d8d15ac0e44651ca09922ae8a7
@ba.gluszczak <ba.gluszczak@gmail.com>
2021-06-17 18:51:32 +0200

    Initialized security, added User entity

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,11 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/api/registration", name="registration")
+     * @Route("/api/registration", name="registration", methods={"POST"})
      */
     public function registration(Request $request): Response
     {
-        dd($request);
+        dd(json_decode($request->getContent()));
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/RegistrationController.php',
